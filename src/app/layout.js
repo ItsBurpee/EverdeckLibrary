dc4c0f559f53d594e923dc76c50a15b36bf0e330
philipdometita <37087348+philipdometita@users.noreply.github.com>
2024-07-24 14:08:54 -0600

    import bootstrap bundle js workaround for later use

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import AppNavbar from "./components/AppNavbar";
 import styles from "./css/mainPage.module.css";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import ImportBsJS from "./components/importBsJs";
+
 export const metadata = {
   title: 'Everdeck Library',
   description: 'A library of games playable by the Everdeck',
@@ -10,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={styles.mainLayout}>
+        <ImportBsJS />
         <AppNavbar />
         {children}
       </body>
