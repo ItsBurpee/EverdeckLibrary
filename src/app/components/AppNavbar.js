e85719dc6495013fd17613c6c28bdc9fe73becf6
Nathan Cruz <cruzb7v94@gmail.com>
2024-07-29 19:12:48 -0600

    Added styling to the Navbar & Search bar AppNavbar.js - Alignment and bg-color SearchBar.js - Box shadow & image alignment

--- src/app/components/AppNavbar.js
+++ src/app/components/AppNavbar.js
@@ -1,6 +1,7 @@
 import { Container, Navbar } from "react-bootstrap";
 import Image from "next/image";
 import { Alegreya_SC } from "next/font/google";
+import styles from "../css/navBar.module.css";
 
 const alegreya_sc = Alegreya_SC({
     weight: '400', 
@@ -14,8 +15,8 @@ export default function AppNavbar() {
 
     return (
         <header>
-            <Navbar className="bg-body-primary">
-                <Container>
+            <Navbar className={styles.navbar}>
+                <Container className={styles.container}>
                     <h1 className={alegreya_sc.className}><Image src={"/everdeck-clam.svg"} width={50} height={50} alt="Website Icon"/> Everdeck Library</h1>
                 </Container>
             </Navbar>
--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -2,6 +2,7 @@
 
 import { Button, Form, InputGroup } from "react-bootstrap"
 import Image from "next/image"
+import styles from "../css/searchBar.module.css";
 
 export default function SearchBar() {
     /*TO-DO: 
@@ -9,14 +10,14 @@ export default function SearchBar() {
     */
 
     return (
-        <div className="search-bar">
-            <InputGroup className="p-3">
+        <div className="p-3">
+            <InputGroup className={styles.searchField}>
                 <Form.Control
                     placeholder="Search" 
                     aria-label="Game Library Search Field"
                     aria-describedby="button-search"
                 />
-                <Button id="button-search">
+                <Button className={styles.searchButton} id="button-search">
                     <Image src={"/search-svgrepo-com.svg"} width={20} height={20} alt="Search button"/> 
                 </Button>
             </InputGroup>
