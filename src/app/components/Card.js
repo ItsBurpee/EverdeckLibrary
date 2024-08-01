828b933b8ebb6360e263a2f3cee18115004f34c2
philipdometita <37087348+philipdometita@users.noreply.github.com>
2024-08-01 14:36:45 -0600

    fixed warning needing 2 taps to open and centered title on mobile

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -80,21 +80,21 @@ export default function Card(props) {
             <div id="game-warning-button">
                 {   //create different overlays if warning is component || mapping || both 
                     props.gameWarning == "component" &&
-                        <OverlayTrigger placement="left" trigger={["click","focus", "hover"]} overlay={componentWarning} >
+                        <OverlayTrigger placement="left" trigger={["click", "hover"]} overlay={componentWarning} >
                             <Button className={`${styles.alertCircle} ${styles.alertCircleComponent}`} >!</Button>
                         </OverlayTrigger>
                     
                     || 
 
                     props.gameWarning == "mapping" &&
-                        <OverlayTrigger placement="left" trigger={["click","focus", "hover"]} overlay={mappingWarning} >
+                        <OverlayTrigger placement="left" trigger={["click", "hover"]} overlay={mappingWarning} >
                             <Button className={`${styles.alertCircle} ${styles.alertCircleMapping}`} >!</Button>
                         </OverlayTrigger>
                         
                     ||
 
                     props.gameWarning == "both" &&
-                        <OverlayTrigger placement="left" trigger={["click","focus", "hover"]} overlay={bothWarning} >
+                        <OverlayTrigger placement="left" trigger={["click", "hover"]} overlay={bothWarning} >
                             <Button className={`${styles.alertCircle} ${styles.alertCircleBoth}`}>!</Button>
                         </OverlayTrigger>
                 }
