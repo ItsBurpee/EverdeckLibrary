fd05342a6d06569cfe0e2ad05f80df89e9ed0132
philipdometita <37087348+philipdometita@users.noreply.github.com>
2024-08-06 16:01:10 -0600

    commented out string replaceAll that was causing issues

--- src/pages/[gameName].js
+++ src/pages/[gameName].js
@@ -2,7 +2,9 @@ import { useRouter } from "next/router";
 
 const Game = () => {
     const router = useRouter();
-    const gameName = router.query.gameName.replaceAll("_", " ");
+    // const gameName = router.query.gameName.replaceAll("_", " ");
+    const gameName = router.query.gameName;
+    console.log(router.query.gameName);
     return (
         <h1>Game: { gameName }</h1>
     );
