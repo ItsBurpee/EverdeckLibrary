18791f822fed545fa4b92e5a0b572f11572d0ddc
philipdometita <37087348+philipdometita@users.noreply.github.com>
2024-08-17 20:04:06 -0600

    card counts and table to grid

--- src/app/components/TableSection.js
+++ src/app/components/TableSection.js
@@ -34,6 +34,21 @@ export default function TableSection({
         }
     }
     
+    // object to hole counts for cards of each card zone
+    let cardCounts = {}
+
+    let totalCards = 0;
+    // initialize object with keys that are the card zone ids
+    cardZones.forEach(cardZone => {
+        cardCounts = {...cardCounts, [cardZone._id]: 0}
+    })
+    
+    // loop through the card table and increment proper card zone count
+    cardTable.forEach(card => {
+        cardCounts[card.cardZone]++;
+        totalCards++;
+    });
+
     return (
             <div className={styles.tableSection}>
                 <h1>Card Table</h1>
@@ -69,24 +84,30 @@ export default function TableSection({
                     />
                 </div>
                 <div className={styles.cardsBoxes}>
-                    <p><b>Total Cards: 60   Property Cards: 30   Currency Cards: 30</b></p>
+                {/* <p><b>Total Cards: {totalCards}   Property Cards: {}   Currency Cards: 30</b></p> */}
+                    <p><b>{`Total Cards: ${totalCards}`}</b></p>
+                    
                     <div className={styles.cardZoneBoxes}>
                         {
                             cardZones.map((zone, index) => (
-                                <CardZoneBox key={zone._id} 
-                                boxColorIndex={index} 
-                                cardZone={zone} 
-                                activeCardZone={activeCardZone}
-                                assignActiveCardZone={assignActiveCardZone}
-                                />
+                                <div key={zone._id}>
+                                    <p><b>{`${zone.name}: ${cardCounts[zone._id]}`}</b></p>
+                                    <CardZoneBox
+                                         
+                                        boxColorIndex={index} 
+                                        cardZone={zone} 
+                                        activeCardZone={activeCardZone}
+                                        assignActiveCardZone={assignActiveCardZone}
+                                    />
+                                </div>
                             ))
                         }
                     </div>
                 </div>
                 <h3>Extra Components</h3>
                     <ul>
-                        {extComponents.map((comp) => {
-                            return <li key={comp}>{comp}</li>
+                        {extComponents.map((comp, index) => {
+                            return <li key={index}>{comp}</li>
                         })}
                     </ul>
             </div>
