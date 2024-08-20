824e75b29b9ef735e82f12f406dc441659b6c458
Nathan Cruz <cruzb7v94@gmail.com>
2024-08-20 16:30:05 -0600

    All card components can be highlighted

--- src/app/components/CardZone.js
+++ src/app/components/CardZone.js
@@ -17,7 +17,7 @@ export default function CardZone({
         "Dots": styles.compDots,
         "Sequence Number": styles.compSeqNum,
         "Border": styles.compBorder,
-        "Picture": styles.compImg,
+        "Image": styles.compImg,
         "Name": styles.compName
     }
 
@@ -35,53 +35,6 @@ export default function CardZone({
         Yellow: styles.cardZoneImg
     }
 
-    /*
-    useEffect(() => {
-        const highlightRed = document.getElementById("highlightRed");
-        const highlightBlue = document.getElementById("highlightBlue");
-        const highlightYellow = document.getElementById("highlightYellow");
-        const cardZoneRed = document.getElementById("cardZoneRed");
-        const cardZoneBlue = document.getElementById("cardZoneBlue");
-        const cardZoneYellow = document.getElementById("cardZoneYellow");
-
-        if (cardZoneRed && highlightRed) {
-            cardZoneRed.addEventListener("mouseenter", () => {
-                highlightRed.classList.add(styles.showHighlight);
-            })
-            cardZoneRed.addEventListener("mouseleave", () => {
-                highlightRed.classList.remove(styles.showHighlight);
-            })
-        }
-
-        if (cardZoneRed && highlightRed) {
-            cardZoneRed.addEventListener("mouseenter", () => {
-                highlightRed.classList.add(styles.showHighlight);
-            })
-            cardZoneRed.addEventListener("mouseleave", () => {
-                highlightRed.classList.remove(styles.showHighlight);
-            })
-        }
-    
-        if (cardZoneBlue && highlightBlue) {
-            cardZoneBlue.addEventListener("mouseenter", () => {
-                highlightBlue.classList.add(styles.showHighlight);
-            })
-            cardZoneBlue.addEventListener("mouseleave", () => {
-                highlightBlue.classList.remove(styles.showHighlight);
-            })
-        }
-    
-        if (cardZoneYellow && highlightYellow) {
-            cardZoneYellow.addEventListener("mouseenter", () => {
-                highlightYellow.classList.add(styles.showHighlight);
-            })
-            cardZoneYellow.addEventListener("mouseleave", () => {
-                highlightYellow.classList.remove(styles.showHighlight);
-            })
-        }
-    }, []);
-    */
-
     let cardZoneIndex = 0;
 
     // TODO: apply highlight id's and classNames based on card zone cardComponents
@@ -97,32 +50,16 @@ export default function CardZone({
                         const zoneKey = "highlight"+currentColor;
                         const cardZoneStyle = cardZoneColorStyle[currentColor];
                         cardZoneIndex++;
-                        
-                        return (
-                            <div 
-                                key={zoneKey}
-                                className={`${styles.cardZoneHighlight} ${cardZoneStyle} ${tempZoneTarget[currentColor]} ${(activeCardZones.includes(cardZone._id))  && styles.showHighlight}`}
-                            >
-                            </div>
-                        )
+                        return (cardZone.cardComponents.map((cardComponent) => {
+                            return (
+                                <div 
+                                    key={zoneKey+cardComponent}
+                                    className={`${styles.cardZoneHighlight} ${cardZoneStyle} ${cardComponentsZones[cardComponent]} ${(activeCardZones.includes(cardZone._id))  && styles.showHighlight}`}
+                                >
+                                </div>
+                            )
+                        }))
                     })}
-                    {/*
-                    <div 
-                        id="highlightRed"
-                        className={`${styles.cardZoneHighlight} ${styles.cardZoneRed} ${styles.cardZoneSeq}`}
-                    >
-                    </div>
-                    <div 
-                        id="highlightBlue"
-                        className={`${styles.cardZoneHighlight} ${styles.cardZoneBlue} ${styles.cardZoneRaS}`}
-                    >
-                    </div>
-                    <div 
-                        id="highlightYellow"
-                        className={`${styles.cardZoneHighlight} ${styles.cardZoneYellow} ${styles.cardZoneImg}`}
-                    >
-                    </div>
-                    */}
                 </div>
                 <div className={styles.cardZoneBoxes}>
                 {
