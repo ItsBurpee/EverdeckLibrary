ad242d015aaf746d3fb7e1530294b9e0f2feca11
Nathan Cruz <cruzb7v94@gmail.com>
2024-08-29 19:25:34 -0600

    Adjusted and cleaned up the About page Other Changes - AppFooter.js   - Adjusted format     - Will still break at smaller resolutions

--- src/app/components/AppFooter.js
+++ src/app/components/AppFooter.js
@@ -5,9 +5,13 @@ export default function AppNavbar({gameTitleProp}) {
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
-                <p className={styles.names}>Created by Nathan, Carl, and Philip</p>
-                <a href={"https://github.com/ItsBurpee/EverdeckLibrary"} className={styles.git} target="_blank">Github</a>
-                <Link href="/about" className={styles.about}>About Us</Link>
+                <p className={styles.names}>
+                    Created by Nathan, Carl, and Philip
+                    &ensp;/&ensp;
+                    <a href={"https://github.com/ItsBurpee/EverdeckLibrary"} className={styles.git} target="_blank">Github</a>
+                    &ensp;/&ensp;
+                    <Link href="/about" className={styles.about}>About Us</Link>
+                </p>
             </div>
         </footer>
     )
--- src/app/page scripts/AboutPage.js
+++ src/app/page scripts/AboutPage.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AppFooter from "../components/AppFooter";
 import AppNavbar from "../components/AppNavbar";
 import styles from "../pages css/aboutPage.module.css";
@@ -14,27 +15,53 @@ const AboutPage = () => {
             <div className={styles.mainPage}>
                 <h1>About The Everdeck</h1>
                 <p>Designed by Wilhelm Su, the Everdeck is a card game system jam packed with card information patterns usable across various traditional and modern games. Each of the card components have been “designed with a ruthless combinatorial efficiency”; the extra glyphs aren’t half-heartedly placed on each card but rather carefully organized in relation to the rest of the 120 cards within the Everdeck resulting in an arrangement that’s “beautiful and practical”. If you want to learn about more of the Everdeck, you can check the links below:</p>
-                <a href={"https://boardgamegeek.com/boardgame/291951/the-everdeck"} className={styles.bgg} >BoardGameGeek Page<br></br><br></br></a>
-                <a href={"https://thewrongtools.wordpress.com/2019/10/10/the-everdeck/"} className={styles.wordpress}>Wordpress Design Page</a>
+                <p><a href={"https://boardgamegeek.com/boardgame/291951/the-everdeck"} className={styles.links} >BoardGameGeek Page</a>&ensp;/&ensp;<a href={"https://thewrongtools.wordpress.com/2019/10/10/the-everdeck/"} className={styles.links}>Wordpress Design Page</a></p>
                 <div>
-                    <h1><br></br>About the App</h1>
-                    <p>	The Everdeck Library was developed as an alternative archiving option for the various games playable by the Everdeck. Most of the Everdeck’s game lists are available on online forums such as BGG and Reddit. The Everdeck Library takes the format of those game lists and expands upon them by providing in-depth information and interactivity from being able to filter out certain games to knowing how the Everdeck maps to the game’s cards and rules. Sifting through the Everdeck’s 120 cards for the game you’re playing is quite involved and forgetting a card or not knowing what a card does can heavily hinder gameplay experience. Tabletop games are a way to gather people around and the last thing you want is fumbling around with rules and setup.</p>
+                    <h1>About the App</h1>
+                    <p>	
+                        The Everdeck Library was developed as an alternative archiving option for the various games playable by the Everdeck. 
+                        Most of the Everdeck’s game lists are available on online forums such as BGG and Reddit. 
+                        The Everdeck Library takes the format of those game lists and expands upon them by providing in-depth information and interactivity from being able to filter out certain games to knowing how the Everdeck maps to the game’s cards and rules. 
+                        Sifting through the Everdeck’s 120 cards for the game you’re playing is quite involved and forgetting a card or not knowing what a card does can heavily hinder gameplay experience. 
+                        Tabletop games are a way to gather people around and the last thing you want is fumbling around with rules and setup.
+                    </p>
+                </div>
+                <div className={styles.sourceDiv}>
+                    <h1>Game Source List</h1>
+                    <a href={"https://boardgamegeek.com/boardgame/291951/the-everdeck"} className={styles.links} >Reddit: Crowdsourcing Everdeck Instructions</a>
+                    <br/>
+                    <a href={"https://boardgamegeek.com/boardgame/291951/the-everdeck"} className={styles.links} >BGG: Everdeck Games Compilation</a>
+                    <br/>
+                    <p>BGG: The Everdeck Companion:&ensp;
+                        <a href={"https://boardgamegeek.com/filepage/263177/the-everdeck-companion-vol-1"} className={styles.links} >1</a>&ensp;
+                        <a href={"https://boardgamegeek.com/filepage/279974/the-everdeck-companion-vol-2"} className={styles.links} >2</a>&ensp;
+                        <a href={"https://boardgamegeek.com/filepage/281080/the-everdeck-companion-vol-3"} className={styles.links} >3</a>
+                    </p>
+                    <p>Nathan's Personal List of Everdeck Games</p>
+                    <br/>
+                    <a href={"https://boardgamegeek.com/"} target="_blank">
+                    <Image
+                        src={"/poweredByBGG.webp"}
+                        alt="Powered by BGG"
+                        height={75}
+                        width={250}
+                    />
+                    </a>
                 </div>
                 <div>  
-                    <h1><br></br>License Information</h1>
-                    <p>MIT License
-                    <br></br><br></br>Copyright (c) 2024 Nathan Cruz, Carl Lyss, Philip Dometita <br></br><br></br>
-
-                        Permission is hereby granted, free of charge, to any person obtaining a copy
+                    <h1>License Information</h1>
+                    <p>MIT License</p>
+                    <p>Copyright (c) 2024 Nathan Cruz, Carl Lyss, Philip Dometita</p>
+                    <p>Permission is hereby granted, free of charge, to any person obtaining a copy
                         of this software and associated documentation files (the "Software"), to deal
                         in the Software without restriction, including without limitation the rights
                         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
                         copies of the Software, and to permit persons to whom the Software is
-                        furnished to do so, subject to the following conditions:<br></br><br></br>
-
+                        furnished to do so, subject to the following conditions:
+                        <br/><br/>
                         The above copyright notice and this permission notice shall be included in all
-                        copies or substantial portions of the Software.<br></br><br></br>
-
+                        copies or substantial portions of the Software.
+                        <br/><br/>
                         THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
                         IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
                         FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,7 +69,7 @@ const AboutPage = () => {
                         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
                         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
                         SOFTWARE.
-                        </p>
+                    </p>
                 </div>
             </div>
             <AppFooter />
